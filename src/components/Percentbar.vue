3e470db315b1997fe2552eb47d586eb5f908545d
Ares <949959378@qq.com>
2020-07-08 14:53:07 +0800

    -

---
+++ src/components/Percentbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="percentbar">
+      <div class="percentbar--outer">
+          <div class="percentbar--inner">
+              
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
